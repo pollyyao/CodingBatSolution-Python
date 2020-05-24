1ce7e272910472d991f406ae0e46eb9ef481eeec
Polly Yao <pollyyao126@hotmail.com>
2020-05-23 21:47:33 -0400

    Create string_times.py

---
+++ Warmup-2/string_times.py
@@ -0,0 +1,19 @@
+"""
+
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+
+"""
+
+def string_times(str, n):
+  
+  result =""
+  for i in range(n):
+    result = result + str
+    
+  return result
+  
