961676f2f17e3eb4c7e56376a0577a98eaebcecc
Polly Yao <pollyyao126@hotmail.com>
2020-05-28 11:24:29 -0400

    Create big_diff.py

---
+++ List-2/big_diff.py
@@ -0,0 +1,34 @@
+"""
+
+Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array. 
+Note: the built-in min(v1, v2) and max(v1, v2) functions return the smaller or larger of two values.
+
+
+big_diff([10, 3, 5, 6]) → 7
+big_diff([7, 2, 10, 9]) → 8
+big_diff([2, 10, 7, 2]) → 8
+
+"""
+
+def big_diff(nums):
+  
+  nowMin = nums[0]
+  
+  nowMax = nums[0]
+  
+  
+  
+  for i in range(len(nums)-1):
+    tmp = min(nowMin, nums[i+1])
+    
+    nowMin = tmp
+    
+    
+  for i in range(len(nums)-1):
+    tmp = max(nowMax, nums[i+1])
+    
+    nowMax = tmp
+    
+  diff = nowMax-nowMin
+  
+  return diff
