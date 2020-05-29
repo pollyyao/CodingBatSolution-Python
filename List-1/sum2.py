f549a9f7fe768f9c685fdf5ace2ff65f51cdacc8
Polly Yao <pollyyao126@hotmail.com>
2020-05-29 10:19:03 -0400

    Update sum2.py

--- List-1/sum2.py
+++ List-1/sum2.py
@@ -10,11 +10,8 @@ sum2([1, 1, 1, 1]) → 2
 """
 
 def sum2(nums):
-  if len(nums) == 0:
-    return 0
-  else:
-    if len(nums)<2:
-      return sum(nums)
+  if len(nums)<2:
+    return sum(nums)
       
-    else:
-      return nums[0] + nums[1]
+  else:
+    return nums[0] + nums[1]
